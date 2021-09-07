efa6c3c4cda8b390556bcfdbf853783fc54a3156
Mike Meldgaard <meldgaardmike@gmail.com>
2021-09-07 09:22:20 +0200

    Test script

--- PythonTestContainer/test.py
+++ PythonTestContainer/test.py
@@ -1,6 +1,7 @@
 import socket
 import time
 import random
+from datetime import timedelta, datetime
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(("172.17.0.5", 4242))
@@ -8,19 +9,25 @@ s.connect(("172.17.0.5", 4242))
 start_time = time.time()
 
 start_range = 1
-num_sensors = 1
-num_repeat = 10000
+num_sensors = 25000
+num_repeat = 35000
+
+start_time = (datetime.now() - timedelta(days=365)).timestamp()
+end_time = datetime.now().timestamp()
 
 for x in range(1,num_repeat):
 	str=""
 	time.sleep(1)
-	curr_epoch = int(round(time.time() * 1000))
+	curr_epoch = int(round(random.uniform(start_time, end_time) * 1000))
+	# curr_epoch = int(round(time.time() * 1000))
+
 #	print "======================"
 #	print x
 #	print "Current time " + time.strftime("%X")
 #	print "======================"
+
 	for y in range(start_range,start_range+num_sensors):
-		statement = "put TEST_SENSOR %d %d status=good\n" %(curr_epoch,random.randint(1,500))
+		statement = "put SENSOR_%d %d %d status=good\n" %(y,curr_epoch,random.randint(1,500))
 #		print statement
 		s.send(statement.encode())
 
